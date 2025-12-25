77d6b9579a69a0370426b73d1c285df74d1d5eeb
manel kacem <manelkacem11@gmail.com>
2025-12-25 13:25:23 +0100

    Enhance ui using stack and positionned

--- lib/screens/apod_screen.dart
+++ lib/screens/apod_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:online_training/data/apod_data.dart';
 import 'package:intl/intl.dart';
+import 'package:online_training/widgets/apod_widget.dart';
 
 class ApodScreen extends StatelessWidget {
   const ApodScreen({super.key});
@@ -15,38 +16,45 @@ class ApodScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: apods.length,
         itemBuilder: (context, index) {
-          return SizedBox(
-            height: MediaQuery.sizeOf(context).height * 0.5,
-            child: Card(
-              color: Colors.orange.shade100,
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16.0),
-              ),
-              child: Padding(
-                padding: const .all(8.0),
-                child: Column(
-                  spacing: 8.0,
-                  crossAxisAlignment: .start,
-                  children: [
-                    Text(apods[index].title),
-                    Text(
-                      DateFormat.yMMMMEEEEd().format(apods[index].date),
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    // Text(apod.date.add(Duration(days: 1)).toString()),
-                    Image.network(
-                      height: 300,
-                      apods[index].url,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Text("Error showing the image from network.");
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ApodWidget(apod: apods[index]),
           );
+          // VERSION 1 OF UI
+          // SizedBox(
+          //   height: MediaQuery.sizeOf(context).height * 0.5,
+          //   child: Card(
+          //     color: Colors.orange.shade100,
+          //     elevation: 4,
+          //     shape: RoundedRectangleBorder(
+          //       borderRadius: BorderRadius.circular(16.0),
+          //     ),
+          //     child: Padding(
+          //       padding: const .all(8.0),
+          //       child: Column(
+          //         spacing: 8.0,
+          //         crossAxisAlignment: .start,
+          //         children: [
+          //           Text(apods[index].title),
+          //           Text(
+          //             DateFormat.yMMMMEEEEd().format(apods[index].date),
+          //             style: TextStyle(fontSize: 12),
+          //           ),
+          //           AspectRatio(
+          //             aspectRatio: 16 / 9,
+          //             child: Image.network(
+          //               height: 300,
+          //               apods[index].url,
+          //               errorBuilder: (context, error, stackTrace) {
+          //                 return Text("Error showing the image from network.");
+          //               },
+          //             ),
+          //           ),
+          //         ],
+          //       ),
+          //     ),
+          //   ),
+          // );
         },
       ),
     );
